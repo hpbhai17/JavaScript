7542384ca87ce8b1ec466cd20884f2bb9ed054ea
hpbhai17 <hpbhai17@gmail.com>
2026-08-15 13:52:52 +0000

    update

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -21,4 +21,4 @@ console.log(accountId);
     Prefer not to use var 
     because of issue of block scope and functional scope  
 */
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) 
