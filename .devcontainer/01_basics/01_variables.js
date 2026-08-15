eeb5684b7db8ed2694dc70dcbdd57bac2501ba11
hpbhai17 <hpbhai17@gmail.com>
2026-08-15 08:05:26 +0000

    update

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,28 @@
+/*const a = 1500
+var b = "Hello"
+let c = 30
+accountNo = 4472
+
+console.table(a,)*/
+
+const accountId = 1144335
+let accountEmail = "Hello@gamil.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+accountEmail = "Hy@gamil.com"
+accountPassword = "54321"
+accountCity = "Bangluru"
+// accountId = 2          //  not allowed to change constant value !
+console.log(accountId);
+
+/*
+    Prefer not to use var 
+    because of issue of block scope and functional scope  
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+
+
